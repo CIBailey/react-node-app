e1aa56bb6e0c4139e2f9a59c7ade8720d5f003ea
Christina <christinaibailey@live.com>
2021-05-13 00:21:34 +0200

    init

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((resp) => resp.json())
+      .then((data) => setData(data.message))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
