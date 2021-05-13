a1ad6932ca2751f7e858745b69723d822242acb2
Christina <christinaibailey@live.com>
2021-05-13 15:59:45 +0200

    chore: add database

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # react-node-app
+
+## install and setup :
+
+Run \$ npm install
+
+Seed movies
+\$ node src/script/seed.js
+
+Start the server :
+\$ npm start
+
+## Api endpoints
+
+### GET requests
+
+### POST requests
---
+++ app.js
@@ -0,0 +1,19 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "react-node-movies";
+const client = new MongoClient(url);
+// Use connect method to connect to the server
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  console.log(db);
+
+  client.close();
+});
