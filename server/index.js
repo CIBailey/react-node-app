63d4e13601ef5b7d3eb3cd729f26b4a003b2756d
Christina <christinaibailey@live.com>
2021-05-13 14:22:17 +0200

    chore: cleanup files

--- server/index.js
+++ server/index.js
@@ -1,22 +1,18 @@
-// const express = require("express");
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // app.get("/api", (req, res) => {
 //   res.json({ message: "Hello from server!" });
 // });
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
-const path = require("path");
-const express = require("express");
-
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// // Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
